e2d4b992222aae2e5e633c28bbe4e75180562cb4
Oleksandr Ivankiv <ivankiv.frostkor@gmail.com>
2024-08-19 17:53:55 +0300

    Added better routing

--- BankChecker/Controllers/ExchangeRateController.cs
+++ BankChecker/Controllers/ExchangeRateController.cs
@@ -6,23 +6,26 @@ using System.Threading.Tasks;
 
 namespace BankChecker.Controllers
 {
+    [Route("ExchangeRate")]
     public class ExchangeRateController : Controller
     {
         private readonly IExchangeRateService _exchangeRateService;
         private readonly AppDbContext _context;
-
         public ExchangeRateController(IExchangeRateService exchangeRateService, AppDbContext context)
         {
             _exchangeRateService = exchangeRateService;
             _context = context;
 
         }
+
+        [HttpGet("BankList")]
         public async Task<IActionResult> BankList()
         {
             var exchangeRates = await _exchangeRateService.GetExchangeRatesAsync();
             return View(exchangeRates);
         }
 
+        [HttpGet("ExchangeDetails/{bankName}/{currencyCode}")]
         public async Task<IActionResult> ExchangeDetails(string bankName, string currencyCode)
         {
             var bank = await _context.FullExchangeRates
